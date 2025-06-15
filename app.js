d6765830dbdc015ce04c79f8abd2cbdd86538356
delia-elena-barbuta <delia-elena.barbuta@academic.tuiasi.ro>
2025-06-15 23:13:43 +0300

    Created app structure and node server

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser')
+const app = express();
+const port = 6789;
+app.set('view engine', 'ejs');
+
+app.use(expressLayouts);
+app.use(express.static('public'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.listen(port, () => console.log(`Server is running at http://localhost:${port}/`));
